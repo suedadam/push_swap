3d938345dc629733aa162cf5543f98d097381eb3
Adam Syed <suedadam@gmail.com>
2017-12-21 00:00:27 -0800

    Concept

--- swapper/main.c
+++ swapper/main.c
@@ -6,7 +6,7 @@
 /*   By: suedadam <suedadam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/07 14:59:11 by asyed             #+#    #+#             */
-/*   Updated: 2017/12/20 17:56:16 by suedadam         ###   ########.fr       */
+/*   Updated: 2017/12/20 23:58:33 by suedadam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -177,16 +177,31 @@ void	top_largest(t_link **stack)
 	// 	printf("\e[1;31mKO\n\e[0m");
 }
 
+int		list_length(t_link *stack)
+{
+	int	i;
+
+	i = 0;
+	while (stack)
+	{
+		i++;
+		stack = stack->next;
+	}
+	return (i);
+}
+
 int		move_calc(t_link **stack_a, t_link **stack_b)
 {
 	static int	max = 0;
 	t_link		*itt_s;
 	int			num;
 	int			ops;
+	int			length;
 
 	num = (*stack_a)->n;
 	itt_s = (*stack_b);
 	ops = 0;
+	length = list_length(*stack_b);
 	if (!itt_s || itt_s->prev == itt_s
 		// (itt_s->next == itt_s->prev) //2 are placed.
 	)
@@ -211,6 +226,7 @@ int		move_calc(t_link **stack_a, t_link **stack_b)
 			// printf("\e[1;36m{%d} (%d || %d || %d)\e[0m\n", num, (itt_s->prev->n > num && num > itt_s->n), (itt_s->n == max && num < itt_s->prev->n), (itt_s->n == max && num > max));
 			// max = (num > max) ? num : max;
 			(*stack_a)->moves += ops;
+			(*stack_a)->rev_moves += (length - ops);
 			return (0);
 		}
 		ops++;
@@ -231,7 +247,10 @@ void	clear_moves(t_link **stack_a)
 
 	while (save)
 	{
-		save->moves = i++;
+		save->p_steps = i++;
+		save->moves = 0;
+		save->rev_moves = 0;
+		save->r_dir = 0;
 		save = save->next;
 	}
 }
@@ -258,12 +277,15 @@ int		pl_in_stack(int y, t_link *check)
 	return (i);
 }
 
-void	rotate_x(t_link	**stack, int i, char *id)
+void	newrotate_x(t_link	**stack, int i, char *id, short rev)
 {
 	while (i-- > 0)
 	{
 		printf("%s\n", id);
-		rot_a(stack, NULL);
+		if (rev)
+			rev_rot_a(stack, NULL);
+		else
+			rot_a(stack, NULL);
 	}
 }
 
@@ -272,43 +294,137 @@ void	print_stack(t_link *stack, char *prefix)
 	while (stack)
 	{
 		if (prefix)
-			printf("%s(%d) %d[%d] (%d)\e[0m\n", prefix, stack->prev->n, stack->n, stack->moves, (stack->next) ? stack->next->n : 0);
+			printf("%s(%d) %d[%d;%d] (%d)\e[0m\n", prefix, stack->prev->n, stack->n, stack->p_steps, stack->moves, (stack->next) ? stack->next->n : 0);
 		else
-			printf("(%d) %d[%d] (%d)\e[0m\n", stack->prev->n, stack->n, stack->moves,(stack->next) ? stack->next->n : 0);
+			printf("(%d) %d[%d;%d] (%d)\e[0m\n", stack->prev->n, stack->n, stack->p_steps, stack->moves,(stack->next) ? stack->next->n : 0);
 		stack = stack->next;
+	}
+}
+
+void	ammor_rotate_x(t_link **stack_a, t_link **stack_b, int i, short rev)
+{
+	while (i-- > 0)
+	{
+		if (rev)
+		{
+			printf("rrr\n");
+			rev_rot_ab(stack_a, stack_b);
+		}
+		else
+		{
+			printf("rr\n");
+			rot_ab(stack_a, stack_b);			
+		}
+
 	}
 }
 
 void	lowest_res(t_link **stack_a, t_link **stack_b)
 {
-	int		i;
+	int		length;
 	t_link	*tmp;
 	t_link	*copy;
 
 	tmp = (*stack_a);
 	copy = tmp;
-	i = 0;
+	length = 0;
 	while (tmp)
 	{
-		if (tmp->moves < copy->moves)
+		if ((tmp->p_steps + ((tmp->r_dir) ? tmp->rev_moves : tmp->moves)) < (copy->p_steps + ((copy->r_dir) ? copy->rev_moves : copy->moves)))
 			copy = tmp;
-		tmp->moves -= i++;
 		tmp = tmp->next;
 	}
-	i = pl_in_stack(copy->n, (*stack_a));
-	rotate_x(stack_a, i, "ra");
+	// i = pl_in_stack(copy->n, (*stack_a));
+	if (copy->moves && copy->p_steps && copy->moves > copy->p_steps && !copy->r_dir)
+	{
+		ammor_rotate_x(stack_a, stack_b, copy->p_steps, 0);
+		copy->moves -= copy->p_steps;
+	}
+	else if (copy->moves && copy->p_steps && copy->moves < copy->p_steps && !copy->r_dir)
+	{
+		// if (copy->n == 54690)
+		// {
+		// 	printf("hi (%d -= %d)\n", copy->p_steps, copy->moves);
+		// }
+		ammor_rotate_x(stack_a, stack_b, copy->moves, 0);
+		copy->p_steps -= copy->moves;
+		newrotate_x(stack_a, copy->p_steps, "ra", 0);
+		copy->moves = 0;
+	}
+	// else if (copy->r_dir && copy->rev_moves && copy->p_steps && copy->rev_moves > copy->p_steps)
+	// {
+	// 	length = list_length(*stack_a);
+	// 	copy->p_steps = (length - copy->p_steps);
+	// 	ammor_rotate_x(stack_a, stack_b, copy->moves, 1);
+	// 	copy->rev_moves -= copy->p_steps;
+	// }
+	else
+	{
+		if (copy->r_dir && copy->rev_moves && copy->p_steps)
+		{
+			length = list_length(*stack_a);
+			if (copy->rev_moves >= (length - copy->p_steps))
+			{
+				copy->p_steps = (length - copy->p_steps);
+				ammor_rotate_x(stack_a, stack_b, copy->p_steps, 1);
+				copy->rev_moves -= copy->p_steps;
+			}
+			else if (copy->rev_moves < (length - copy->p_steps)) //Disabled
+			{
+				copy->p_steps = ((length - copy->p_steps) - copy->rev_moves);
+				ammor_rotate_x(stack_a, stack_b, copy->rev_moves, 1);
+				if (copy->p_steps > 0)
+				{
+					printf("rra * %d\n", copy->p_steps);
+					newrotate_x(stack_a, copy->p_steps, "rra", 1);
+				}
+				copy->rev_moves = 0;
+				// copy->rev_moves -= copy->p_steps;
+			}
+			else
+			{
+				printf("never got here\n");
+				newrotate_x(stack_a, copy->p_steps, "ra", 0);
+			}
+		}
+		else
+			newrotate_x(stack_a, copy->p_steps, "ra", 0);
+		// if (copy->n == 54690)
+		// {
+		// 	printf("bitch pls\n");
+		// }
+		// printf("%d\n", copy->r_dir);
+		// newrotate_x(stack_a, copy->p_steps, "ra", 0);
+	}
+	// printf("\e[1;33m{RX} %d->p_steps %d\n", copy->n, copy->p_steps);
+	// print_stack(*stack_a, "\e[1;33m{RX}");
 	if (*stack_a == copy)
 	{
 		// printf("%d->moves (%d)\n", copy->n, copy->moves);
-		rotate_x(stack_b, copy->moves, "rb");
+		if (copy->r_dir)
+		{
+			// printf("\e[1;34mreverse detected %d (%d)\e[0m\n", copy->n, copy->rev_moves);
+			// printf("Pre-rotate: (%d)\n", copy->n);
+			// print_stack(*stack_b, "\e[1;35m{Pre-Push}");
+			newrotate_x(stack_b, copy->rev_moves, "rrb", 1);
+		}
+		else
+			newrotate_x(stack_b, copy->moves, "rb", 0);
+			// rotate_x(stack_b, copy->moves, "rb");
 		printf("pb\n");
 		push_b(stack_a, stack_b);
-		// print_stack(*stack_b, "\e[1;33m{PS}");
 	}
 	else
 	{
-		printf("Mis-calculation.\n");
+		//Find this error.
+		printf("Mis-calculation. (%d || %d) (%d instead of %d)\n", (*stack_a)->prev == copy, (((*stack_a)->next) ? (*stack_a)->next == copy : 0), (*stack_a)->n, copy->n);
 	}
+}
+
+void	path_choose(t_link *stack)
+{
+	if (stack->rev_moves > 0 && stack->rev_moves < stack->moves)
+		stack->r_dir = 1;
 }
 
 /*
@@ -336,9 +452,9 @@ void	push_min(t_link **stack_a, t_link **stack_b)
 			printf("pb\n");
 			push_b(stack_a, stack_b);
 			top_largest(stack_b);
-			// print_stack(*stack_b, "\e[1;32m{CHECK} ");
 			return ;
 		}
+		path_choose(tmp);
 		tmp = tmp->next;
 	}
 	lowest_res(stack_a, stack_b);
@@ -353,16 +469,6 @@ int		sortMoves(t_link **stack_a)
 		push_min(stack_a, &stack_b);
 	startover(&stack_b);
 	top_largest(&stack_b);
-	// if (rev_sort_check(&stack_b))
-	// {
-	// 	// printf("\e[1;32m{RC} OK\e[0m\n");
-	// 	// print_stack(*stack_b, "\e[1;32m{RC} ");
-	// }
-	// else
-	// {
-	// 	// printf("\e[1;31m{RC} ERROR\e[0m\n");
-	// 	// print_stack(*stack_b, "\e[1;31m{RC} ");
-	// }
 	while (stack_b)
 	{
 		printf("pa\n");
