316dad9d57ff4b35b9aacbf3c925d3262d5b1951
Adam Syed <suedadam@gmail.com>
2017-12-21 22:05:12 -0800

    Hardcoded sorting for <= 3 numbers

--- swapper/main.c
+++ swapper/main.c
@@ -6,7 +6,7 @@
 /*   By: suedadam <suedadam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/07 14:59:11 by asyed             #+#    #+#             */
-/*   Updated: 2017/12/21 18:52:21 by suedadam         ###   ########.fr       */
+/*   Updated: 2017/12/21 22:02:17 by suedadam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,10 +52,7 @@ int		sort_check(t_link **stack_a)
 	while (save && save->next)
 	{
 		if (save->next->n < save->n)
-		{
-			printf("%d < %d\n", save->next->n, save->n);
 			return (0);
-		}
 		save = save->next;
 	}
 	return (1);
@@ -327,72 +324,46 @@ void	lowest_res(t_link **stack_a, t_link **stack_b)
 
 	tmp = (*stack_a);
 	copy = tmp;
-	length = 0;
+	length = list_length(*stack_a);
 	while (tmp)
 	{
-		if ((tmp->p_steps + ((tmp->r_dir) ? tmp->rev_moves : tmp->moves)) < (copy->p_steps + ((copy->r_dir) ? copy->rev_moves : copy->moves)))
-			copy = tmp;
+		if (tmp->r_dir)
+		{
+			length = list_length(*stack_a);
+			tmp->p_steps = (length - tmp->p_steps);
+			if ((tmp->p_steps + tmp->rev_moves) < ((copy->r_dir) ? copy->rev_moves : copy->moves) + copy->p_steps)
+				copy = tmp;
+		}
+		else
+		{
+			if ((tmp->p_steps + tmp->moves) < ((copy->r_dir) ? copy->rev_moves : copy->moves) + copy->p_steps)
+				copy = tmp;
+		}
 		tmp = tmp->next;
 	}
-	// length = list_length(*stack_a);
-	// if (copy->r_dir)
-	// {
-	// 	//forgot to use length;
-	// 	if (copy->moves && copy->p_steps && copy->p_steps >= (length > 2))
-	// 	{
-	// 		copy->p_steps = (length - copy->p_steps);
-	// 		// if (copy->rev_moves >= copy->p_steps)
-	// 		// {
-	// 		// 	newrotate_x(stack_a, copy->rev_moves, "rra", 1);
-	// 		// 	copy->rev_moves -= copy->p_steps;
-	// 		// 	copy->p_steps = 0;
-	// 		// }
-	// 		// else
-	// 		// {
-	// 		// 	newrotate_x(stack_a, copy->rev_moves, "rra", 1);
-	// 		// 	copy->p_steps -= copy->rev_moves;
-	// 		// 	copy->rev_moves = 0;
-	// 		// }
-	// 		newrotate_x(stack_a, copy->p_steps, "rra", 1);
-	// 	}
-	// 	else
-	// 		newrotate_x(stack_a, copy->p_steps, "ra", 0);
-	// }
-	// else
-	// {
-
 	if (copy->r_dir)
 	{
-		length = list_length(*stack_a);
-		if (copy->p_steps && ((length - copy->p_steps) < copy->p_steps))
+		if (copy->rev_moves && copy->p_steps)
 		{
-			if (copy->rev_moves && copy->p_steps)
+			if (copy->rev_moves >= copy->p_steps)
 			{
-				copy->p_steps = (length - copy->p_steps);
-				if (copy->rev_moves >= copy->p_steps)
-				{
-					ammor_rotate_x(stack_a, stack_b, copy->p_steps, 1);
-					copy->rev_moves -= copy->p_steps;
-					copy->p_steps = 0;
-				}
-				else if (copy->rev_moves < copy->p_steps)
-				{
-					ammor_rotate_x(stack_a, stack_b, copy->rev_moves, 1);
-					copy->p_steps -= copy->rev_moves;
-					copy->rev_moves = 0;
-				}
-				else
-				{
-					printf("undefined behavior?\n");
-				}
+				ammor_rotate_x(stack_a, stack_b, copy->p_steps, 1);
+				copy->rev_moves -= copy->p_steps;
+				copy->p_steps = 0;
 			}
-			newrotate_x(stack_a, copy->p_steps, "rra", 1);
-			copy->p_steps = 0;
+			else if (copy->rev_moves < copy->p_steps)
+			{
+				ammor_rotate_x(stack_a, stack_b, copy->rev_moves, 1);
+				copy->p_steps -= copy->rev_moves;
+				copy->rev_moves = 0;
+			}
 		}
+		newrotate_x(stack_a, copy->p_steps, "rra", 1);
+		copy->p_steps = 0;
 	}
 	else
 	{
-		if (!copy->r_dir && copy->moves && copy->p_steps)
+		if (copy->moves && copy->p_steps)
 		{
 			if (copy->moves >= copy->p_steps)
 			{
@@ -405,34 +376,21 @@ void	lowest_res(t_link **stack_a, t_link **stack_b)
 				ammor_rotate_x(stack_a, stack_b, copy->moves, 0);
 				copy->p_steps -= copy->moves;
 				copy->moves = 0;
-			}			
+			}	
 		}
+		newrotate_x(stack_a, copy->p_steps, "ra", 0);
 	}
-	newrotate_x(stack_a, copy->p_steps, "ra", 0);
-	// }
-	// printf("\e[1;33m{RX} %d->p_steps %d\n", copy->n, copy->p_steps);
-	// print_stack(*stack_a, "\e[1;33m{RX}");
 	if (*stack_a == copy)
 	{
-		// printf("%d->moves (%d)\n", copy->n, copy->moves);
 		if (copy->r_dir)
-		{
-			// printf("\e[1;34mreverse detected %d (%d)\e[0m\n", copy->n, copy->rev_moves);
-			// printf("Pre-rotate: (%d)\n", copy->n);
-			// print_stack(*stack_b, "\e[1;35m{Pre-Push}");
 			newrotate_x(stack_b, copy->rev_moves, "rrb", 1);
-		}
 		else
 			newrotate_x(stack_b, copy->moves, "rb", 0);
-			// rotate_x(stack_b, copy->moves, "rb");
 		printf("pb\n");
 		push_b(stack_a, stack_b);
 	}
 	else
-	{
-		//Find this error.
 		printf("Mis-calculation. (%d || %d) (%d instead of %d)\n", (*stack_a)->prev == copy, (((*stack_a)->next) ? (*stack_a)->next == copy : 0), (*stack_a)->n, copy->n);
-	}
 }
 
 void	path_choose(t_link *stack)
@@ -491,6 +449,38 @@ int		sortMoves(t_link **stack_a)
 	return (0);
 }
 
+void	sort_3(t_link **stack_a, int length)
+{
+	int	max;
+
+	if (length == 1)
+		return ;
+	if (length == 2)
+	{
+		printf("ra\n");
+		rot_a(stack_a, NULL);
+	}
+	if (length == 3)
+	{
+		max = pl_in_stack(largest_val(stack_a), *stack_a);
+		if (!max)
+		{
+			printf("ra\n");
+			rot_a(stack_a, NULL);
+		}
+		if (max == 1)
+		{
+			printf("rra\n");
+			rev_rot_a(stack_a, NULL);
+		}
+		if ((*stack_a)->n > (*stack_a)->next->n)
+		{
+			printf("sa\n");
+			swap_a(stack_a, NULL);
+		}
+	}
+}
+
 int	main(int argc, char *argv[])
 {
 	t_link	*stack_a;
@@ -508,13 +498,19 @@ int	main(int argc, char *argv[])
 		write(1, "Error\n", 6);
 		return (-1);
 	}
+	if (sort_check(&stack_a))
+		return (1);
 	// test = stack_a;
 	// while (test)
 	// {
 	// 	printf("(%d) %d (%d)\n", test->prev->n, test->n, (test->next) ? test->next->n : 0);
 	// 	test = test->next;
 	// }
-	sortMoves(&stack_a);
+	i = list_length(stack_a);
+	if (i <= 3)
+		sort_3(&stack_a, i);
+	else
+		sortMoves(&stack_a);
 	printf("\e[1;35m==Final==\e[0m\n");
 	if (sort_check(&stack_a))
 	{
